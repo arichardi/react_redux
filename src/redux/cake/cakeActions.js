e5e54f9c28fe108561632aa342c3d5a8e07212d5
Andre Richardi <a.richardi@gmail.com>
2021-01-27 10:41:51 -0300

    create actions and reducer

---
+++ src/redux/cake/cakeActions.js
@@ -0,0 +1,10 @@
+
+//use the simple export 'cause the archive can have diferent kind of actions related to the cake
+
+
+export const buyCake = () => {
+    return {
+        type: 'BUY_CAKE',
+        info: 'consume a cake of store'
+    }
+}
---
+++ src/redux/cake/cakeTypes.js
@@ -0,0 +1,3 @@
+//this element has the meaning inside the context
+
+export const BUY_CAKE = 'BUY_CAKE'
